c1d57afa75c97bcde0362b6a53273840a7d5ce27
Lam Le <lam.le@2clicksolutions.com>
2019-10-15 18:32:10 +0700

    Update tenant functions, added type and sync out-of-date properties

--- contracts/PharmaChain.sol
+++ contracts/PharmaChain.sol
@@ -1,43 +1,43 @@
 pragma solidity >=0.4.22 <0.6.0;
 
 import './MedicineBatch.sol';
-import './ChainPoint.sol';
+import './Tenant.sol';
 import './MedicineBatchTransfer.sol';
 
 contract PharmaChain {
-    
+
     /** @dev stores medicine batch addresses || chain point addresses || medicine batch transfer by guid */
     mapping(bytes32 => address) public contractAddresses;
-    
+
     /** @dev stores medicine batch transfers by particular medicine batch like using 3-dimensionality array [getKey(medicineBatchId)][chainIndex][transferIndex] */
     mapping(bytes32 => mapping(uint => mapping(uint => MedicineBatchTransfer))) public medicineBatchTransfers;
-    
+
     /** @dev stores chain counters which be use to query transfers */
     mapping(bytes32 => uint) public chainCounters;
-    
+
     /** @dev stores transfer counters which be use to query transfers */
     mapping(bytes32 => mapping(uint => uint)) public transferCounters;
-    
+
     /** @dev stores which addresses are owned by Global administrators */
     mapping (address => bool) public admins;
 
     /** @dev stores the address of the Global Administrator */
     address public globalAdmin;
-    
+
     event AdminAdded(address _address);
     event AdminRemoved(address _address);
     event MedicineBatchReleased(string _guid, address _address);
     event MedicineBatchRemoved(string _guid, address _address);
-    event ChainPointAdded(string _guid, address _address);
-    event ChainPointRemoved(string _guid, address _address);
+    event TenantAdded(string _guid, address _address);
+    event TenantRemoved(string _guid, address _address);
     event MedicineBatchTransferAdded(string _guid, address _address);
     event MedicineBatchTransferRemoved(string _guid, address _address);
-    
+
     constructor() public {
         globalAdmin = msg.sender;
         admins[msg.sender] = true;
     }
-    
+
     // ================Medicine Functions================
     function releaseMedicineBatch(
         string memory _guid,
@@ -47,12 +47,12 @@ contract PharmaChain {
         uint _quantity,
         uint _manufacturingDate,
         uint _expiryDate,
-        MedicineBatch.TypesOfMedicine _typeOfMedicine) 
-        public 
-        onlyAdmin 
+        MedicineBatch.TypesOfMedicine _typeOfMedicine)
+        public
+        onlyAdmin
     {
         bytes32 key = getKey(_guid);
-        
+    
         MedicineBatch newMedicineBatch = new MedicineBatch(
                                             _guid,
                                             _name,
@@ -62,55 +62,55 @@ contract PharmaChain {
                                             _manufacturingDate,
                                             _expiryDate,
                                             _typeOfMedicine);
-                                    
+
         contractAddresses[key] = address(newMedicineBatch);
-                                    
+
         emit MedicineBatchReleased(_guid, address(newMedicineBatch));
     }
-    
+
     function removeMedicineBatch(string memory _guid) public onlyAdmin {
         bytes32 key = getKey(_guid);
-        
+
         MedicineBatch(contractAddresses[key]).destroy();
 
         emit MedicineBatchRemoved(_guid, contractAddresses[key]);
         delete contractAddresses[key];
     }
-    
-    // ================ChainPoint Functions================
-    function addChainPoint(
+
+    // ================Tenant Functions================
+    function addTenant(
         string memory _guid,
         string memory _name,
         string memory _address,
         string memory _phoneNumber,
         string memory _taxCode,
-        string memory _BRCLink,
-        string memory _GPCLink) 
-        public 
-        onlyAdmin 
+        string memory _registrationCode,
+        string memory _goodPractices)
+        public
+        onlyAdmin
     {
         bytes32 key = getKey(_guid);
-        
-        ChainPoint newChainPoint = new ChainPoint(
+
+        Tenant newChainPoint = new Tenant(
                                         _guid,
                                         _name,
                                         _address,
                                         _phoneNumber,
                                         _taxCode,
-                                        _BRCLink,
-                                        _GPCLink);
-                                    
-        contractAddresses[key] = address(newChainPoint);
-                                    
-        emit ChainPointAdded(_guid, address(newChainPoint));
-    }
-    
-    function removeChainPoint(string memory _guid) public onlyAdmin {
-        bytes32 key = getKey(_guid);
-        
-        ChainPoint(contractAddresses[key]).destroy();
+                                        _registrationCode,
+                                        _goodPractices);
 
-        emit ChainPointRemoved(_guid, contractAddresses[key]);
+        contractAddresses[key] = address(newChainPoint);
+
+        emit TenantAdded(_guid, address(newChainPoint));
+    }
+
+    function removeTenant(string memory _guid) public onlyAdmin {
+        bytes32 key = getKey(_guid);
+
+        Tenant(contractAddresses[key]).destroy();
+
+        emit TenantRemoved(_guid, contractAddresses[key]);
         delete contractAddresses[key];
     }
     
@@ -138,16 +138,16 @@ contract PharmaChain {
                                             _quantity);
                                             
         contractAddresses[key] = address(transfer);
-        
+
         medicineBatchTransfers[key][_chainIndex][transferCounter] = transfer;
         transferCounter++;
         if (_chainIndex == chainCounter) {
             chainCounter++;
         }
-        
+
         emit MedicineBatchTransferAdded(_guid, address(transfer));
     }
-    
+
     function getMedicineTransfer(
         string memory _medicineBatchId,
         uint _chainIndex,
@@ -167,7 +167,7 @@ contract PharmaChain {
         emit MedicineBatchTransferRemoved(_guid, contractAddresses[key]);
         delete contractAddresses[key];
     }
-    
+
     // ================Administrator Functions================
     function addAdmin(address _address) public onlyGlobalAdmin {
         admins[_address] = true;
@@ -178,7 +178,7 @@ contract PharmaChain {
         delete admins[_address];
         emit AdminRemoved(_address);
     }
-    
+
     /**
       * Get the address of a internal contract.
       *
@@ -201,7 +201,7 @@ contract PharmaChain {
     function getKey(string memory _guid) public pure returns (bytes32) {
         return sha256(abi.encodePacked(_guid));
     }
-    
+
     // ================Modifiers================
     modifier onlyGlobalAdmin() {
         require(msg.sender == globalAdmin);
